dbc52d3694ea3471cb66f1e52a8e323775c61444
JamesG <debbie1235@hotmail.com>
2020-08-05 20:41:27 +1000

    Started Block Coding

---
+++ Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    public int width { get; set; }
+    public int height { get; set; }
+    public int depth { get; set; }
+
+    public float xPos { get; set; }
+    public float yPos {get; set; }
+    public float zPos {get; set; }
+
+        
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
