093d9391a4f78fc63a8856cc46e3932eb42cbf1d
james-garrett <jkgarrett92@gmail.com>
2020-08-06 08:09:08 +1000

    Block and Tower Skeleton Code

--- Block.cs
+++ Block.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 
-    public int width { get; set; }
-    public int height { get; set; }
-    public int depth { get; set; }
+    /**
+           +--------+
+          /        /|
+         /        / |
+        +--------+  |  
+        |        |  |
+        |        |  +
+ height |        | /
+        |        |/ depth
+        +--------+
+           width
+    **/
+
+// Please change coord stuff as needed to make it usable
+    public float width { get; set; }
+    public float height { get; set; }
+    public float depth { get; set; }
+
+
+    
 
     public float xPos { get; set; }
     public float yPos {get; set; }
     public float zPos {get; set; }
 
+    public bool hasBlockBeenMoved {get; set;} = false;
+    public bool isBlockTouchingGround {get; set;} = false;
+
+
+    public Block(float w, float h, float d, float x, float y, float z) {
+        width = w;
+        height = h;
+        depth = d;
+
+        xPos = x;
+        yPos = y;
+        zPos = z;
+    }
+
+    // Either use some kind of global variable for ground and see if block is touching it (coordinates overlap),
+    // or get args for ground from calling parent and just see if collision has occured with those
+    public bool checkIfBlockIsTouchingGround(/*ground coord args?*/) {
+        if(hasBlockBeenMoved) {
+            // Check for collision
+        }
+    }
         
     void Start()
     {
