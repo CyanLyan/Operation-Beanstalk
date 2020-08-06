c80a1fb9cc5040d14c0858923542b4c7c19b9f07
Josh Rosovske-Kendall <jrk@cyanlyan.co.uk>
2020-08-06 18:12:37 +0100

    Updated gitignore

---
+++ OperationBeanstalk/Assets/Scripts/Pallet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pallet : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public List<Block> members = new List<Block>();
+
+    void Awake()
+    {
+        createPallet(); //Only in awake for debug
+    }
+
+    public void createPallet(float nBlocks = 3f, float blockSpacing = 0.3f)
+    {
+        float start = (nBlocks/2f)-0.5f; //Distance from midpoint of furthest block to center, in blocks
+        GameObject block;
+        float positionOffset;
+        for(float i = -start; i <= start; i += 1f)
+        {
+            block = GameObject.Instantiate(blockPrefab, transform);
+            positionOffset = (block.transform.localScale.z + blockSpacing) * i; //Distance from midpoints, in units
+            block.transform.Translate(  new Vector3( 0f, 0f, positionOffset) );
+            members.Add(block.GetComponent<Block>());
+        }
+    }
+}
---
+++ OperationBeanstalk/Assets/Scripts/Tower.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public int nPallets;
+
+
+    //Function to instantiate pallets vertically, to be written
+}
