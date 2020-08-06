a5b6137507dc9f5835e977cce9def7270d6652b6
Josh Rosovske-Kendall <jrk@cyanlyan.co.uk>
2020-08-06 17:52:22 +0100

    Implemented block prefab and pallet script

---
+++ Operation Beanstalk/Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public bool hasBlockBeenMoved {get; set;} = false;
+    public bool isBlockTouchingGround {get; set;} = false;
+    public static int nBlocksOnGround {get; set;} = 0;
+
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag != "GroundPlane") return; //for now, drop this function for all collisions but that with the ground
+        isBlockTouchingGround = true;
+        nBlocksOnGround++;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.tag != "GroundPlane") return; //for now, drop this function for all collisions but that with the ground
+        isBlockTouchingGround = false;
+        if(nBlocksOnGround-- < 3);
+    }
+}
