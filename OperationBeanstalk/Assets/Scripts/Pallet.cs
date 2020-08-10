97e85765f52073625d7d9babb175989e8d880367
JamesG <debbie1235@hotmail.com>
2020-08-10 21:17:45 +1000

    Hoping this doesn't destroy everything
    Please don't break everything :S

--- OperationBeanstalk/Assets/Scripts/Pallet.cs
+++ OperationBeanstalk/Assets/Scripts/Pallet.cs
@@ -6,20 +6,29 @@ public class Pallet : MonoBehaviour
 {
     public GameObject blockPrefab;
     public List<Block> members = new List<Block>();
+    public float x;
+    public float y;
+    public float z;
+
+    public Pallet(GameObject block, float xPos = 0, float yPos = 0, float zPos = 0, float nBlocks = 3f, float blockSpacing = 0.3f)
+    {
+        
+        createPallet(block);
+    }
 
     void Awake()
     {
-        createPallet(); //Only in awake for debug
+        //0createPallet(block); //Only in awake for debug
     }
 
-    public void createPallet(float nBlocks = 3f, float blockSpacing = 0.3f)
+    public void createPallet(GameObject block, float nBlocks = 3f, float blockSpacing = 0.3f)
     {
         float start = (nBlocks/2f)-0.5f; //Distance from midpoint of furthest block to center, in blocks
-        GameObject block;
+        //GameObject block;
         float positionOffset;
         for(float i = -start; i <= start; i += 1f)
         {
-            block = GameObject.Instantiate(blockPrefab, transform);
+            block = GameObject.Instantiate(block, transform);
             positionOffset = (block.transform.localScale.z + blockSpacing) * i; //Distance from midpoints, in units
             block.transform.Translate(  new Vector3( 0f, 0f, positionOffset) );
             members.Add(block.GetComponent<Block>());
